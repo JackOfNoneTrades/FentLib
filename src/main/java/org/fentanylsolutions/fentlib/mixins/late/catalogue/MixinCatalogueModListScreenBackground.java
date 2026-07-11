f0445c9a472d5168fe677fceeff08b9b5ad7fa8e
jack <181404551+JackOfNoneTrades@users.noreply.github.com>
2026-07-11 13:22:48 +0200

    Fix Catalogue 1.0.8 compatibility (fixes #4)

--- src/main/java/org/fentanylsolutions/fentlib/mixins/late/catalogue/MixinCatalogueModListScreenBackground.java
+++ src/main/java/org/fentanylsolutions/fentlib/mixins/late/catalogue/MixinCatalogueModListScreenBackground.java
@@ -23,7 +23,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.cleanroommc.catalogue.client.IModData;
+import com.cleanroommc.catalogue.client.data.IModData;
 import com.cleanroommc.catalogue.client.screen.CatalogueModListScreen;
 
 @Mixin(CatalogueModListScreen.class)
